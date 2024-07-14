38e10aa802c331b58d0d26de6f0ef1f91e9e21bb
samiam376 <ssheridan211@gmail.com>
2024-07-14 16:10:07 -0700

    messages

--- src/budget_chat.zig
+++ src/budget_chat.zig
@@ -17,3 +17,42 @@ test "announce_names_msg" {
 
     try std.testing.expectEqualStrings("* The room contains: Finn, Franklin, Filip\n", msg);
 }
+
+fn user_joins_msg(allocator: std.mem.Allocator, name: []const u8) ![]u8 {
+    const msg = try std.fmt.allocPrint(allocator, "* {s} has entered the room\n", .{name});
+    return msg;
+}
+
+test "user_join_msg" {
+    const allocator = std.testing.allocator;
+    const msg = try user_joins_msg(allocator, "Bob");
+    defer allocator.free(msg);
+
+    try std.testing.expectEqualStrings("* Bob has entered the room\n", msg);
+}
+
+fn user_leaves_msg(allocator: std.mem.Allocator, name: []const u8) ![]u8 {
+    const msg = try std.fmt.allocPrint(allocator, "* {s} has left the room\n", .{name});
+    return msg;
+}
+
+test "user_leaves_msg" {
+    const allocator = std.testing.allocator;
+    const msg = try user_leaves_msg(allocator, "Bob");
+    defer allocator.free(msg);
+
+    try std.testing.expectEqualStrings("* Bob has left the room\n", msg);
+}
+
+fn fmt_msg(allocator: std.mem.Allocator, user_name: []const u8, msg: []const u8) ![]u8 {
+    const result = try std.fmt.allocPrint(allocator, "[{s}] {s}\n", .{ user_name, msg });
+    return result;
+}
+
+test "fmt_msg" {
+    const allocator = std.testing.allocator;
+    const msg = try fmt_msg(allocator, "Alice", "Hello, World!");
+    defer allocator.free(msg);
+
+    try std.testing.expectEqualStrings("[Alice] Hello, World!\n", msg);
+}
